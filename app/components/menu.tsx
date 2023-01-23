9ff933e1edd85043aee34aa047dbefcd26f3e845
Graham Hagenah <gwhagenah@gmail.com>
2023-01-22 23:24:44 -0500

    testing deploy again

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -153,6 +153,7 @@ export default function CustomizedMenus( props ) {
         </StyledMenu>
         }
         {props.group &&
+        
           <StyledMenu
             id="group-indicator"
             MenuListProps={{
